43cd2f90635574abb94cc138304868aa6364fdc7
Adarsh Suresh <adarshs3@illinois.edu>
2020-12-27 02:56:52 -0600

    Node.js service deployed

---
+++ endpoint/app.js
@@ -0,0 +1,25 @@
+var createError = require('http-errors');
+var express = require('express');
+
+var gmailRouter = require('./routes/gmail');
+
+var app = express();
+
+app.use('/gmail', gmailRouter);
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  // res.render('error');
+});
+
+module.exports = app;
